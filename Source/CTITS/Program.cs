6720e682d1c1de87d2607b33a5622aac1d0853f0
Ruben Verdoes <horsedrowner@gmail.com>
2013-01-28 19:28:57 +0100

    Some more info

--- Source/CTITS/Program.cs
+++ Source/CTITS/Program.cs
@@ -32,9 +32,16 @@ namespace TITS
             ZPlay player = new ZPlay();
 
             Console.WriteLine("Opening {0}...", filename);
+            player.SetSettings(TSettingID.sidAccurateLength, 1);
+            player.SetSettings(TSettingID.sidAccurateSeek, 1);
             player.OpenFile(filename, TStreamFormat.sfAutodetect);
-            player.StartPlayback();
 
+            if (!player.StartPlayback())
+            {
+                throw new Exception(player.GetError());
+            }
+
+            // ID3v2
             TID3Info info = default(TID3Info);
             if (player.LoadID3(TID3Version.id3Version2, ref info) && info.Title.Length > 0)
             {
@@ -44,6 +51,13 @@ namespace TITS
                 Console2.WriteLine(ConsoleColor.Magenta, info.Artist);
             }
 
+            // Get track length
+            TStreamInfo streamInfo = default(TStreamInfo);
+            player.GetStreamInfo(ref streamInfo);
+            TStreamTime totalTime = streamInfo.Length;
+
+            // Playback loop
+            int playcount = 0;
             TStreamStatus status = default(TStreamStatus);
             player.GetStatus(ref status);
             while (status.fPlay)
@@ -63,14 +77,17 @@ namespace TITS
                 // Enqueue for gapless playback
                 if (status.nSongsInQueue == 0)
                 {
+                    playcount++;
                     player.AddFile(filename, TStreamFormat.sfAutodetect);
                 }
 
                 // Display running time
                 TStreamTime time = default(TStreamTime);
                 player.GetPosition(ref time);
+
                 Console2.Write(ConsoleColor.DarkGreen, "Playing ");
-                Console2.Write(ConsoleColor.Green, "{0:00}:{1:00}:{2:00}\r", time.hms.hour, time.hms.minute, time.hms.second);
+                Console2.Write(ConsoleColor.Green, "{0:00}:{1:00}:{2:00} / {3:00}:{4:00}:{5:00} ", time.hms.hour, time.hms.minute, time.hms.second, totalTime.hms.hour, totalTime.hms.minute, totalTime.hms.second);
+                Console2.Write(ConsoleColor.DarkGreen, "({0})\r", playcount);
 
                 System.Threading.Thread.Sleep(10);
                 player.GetStatus(ref status);
