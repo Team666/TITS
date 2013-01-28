2d2b7dc3be865d5d67c3a043906d0e9df3bafcbd
Ruben Verdoes <horsedrowner@gmail.com>
2013-01-28 19:05:26 +0100

    Created projects, gapless playback achieved!

---
+++ Source/CTITS/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using libZPlay;
+
+namespace TITS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "TITS";
+
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("CTITS [filename]\n");
+                Console.WriteLine("\tfilename\tThe name of the file to play.");
+                return;
+            }
+
+            Console2.WriteLine(ConsoleColor.White, "TITS Console");
+            try
+            {
+                StartLoop(args[0]);
+            }
+            catch (Exception ex)
+            {
+                Console2.WriteLine(ConsoleColor.Yellow, ex.ToString());
+            }
+        }
+
+        static void StartLoop(string filename)
+        {
+            ZPlay player = new ZPlay();
+
+            Console.WriteLine("Opening {0}...", filename);
+            player.OpenFile(filename, TStreamFormat.sfAutodetect);
+            player.StartPlayback();
+
+            TID3Info info = default(TID3Info);
+            if (player.LoadID3(TID3Version.id3Version2, ref info) && info.Title.Length > 0)
+            {
+                Console2.Write(ConsoleColor.Gray, "Playing ");
+                Console2.Write(ConsoleColor.Magenta, info.Title);
+                Console2.Write(ConsoleColor.Gray, " - ");
+                Console2.WriteLine(ConsoleColor.Magenta, info.Artist);
+            }
+
+            TStreamStatus status = default(TStreamStatus);
+            player.GetStatus(ref status);
+            while (status.fPlay)
+            {
+                if (status.nSongsInQueue == 0)
+                {
+                    Console2.WriteLine(ConsoleColor.White, "Enqueueing");
+                    player.AddFile(filename, TStreamFormat.sfAutodetect);
+                }
+
+                System.Threading.Thread.Sleep(100);
+                player.GetStatus(ref status);
+            }
+        }
+    }
+}
---
+++ Source/LibTITS/Console.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TITS
+{
+    /// <summary>
+    /// Provides additional functionality to the <see cref="System.Console"/> class.
+    /// </summary>
+    public static class Console2
+    {
+        /// <summary>
+        /// Writes the specified string value to the standard output stream, in the specified color.
+        /// </summary>
+        /// <param name="color">The foreground color to display the value in.</param>
+        /// <param name="value">The value to write.</param>
+        public static void Write(ConsoleColor color, string value)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.Write(value);
+            System.Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Writes the text representation of the specified array of objects to the standard output 
+        /// stream using the specified format information, in the specified color.
+        /// </summary>
+        /// <param name="color">The foreground color to display the value in.</param>
+        /// <param name="format">A composite format string.</param>
+        /// <param name="arg">An array of objects to write using <paramref name="format"/>.</param>
+        public static void Write(ConsoleColor color, string format, params object[] arg)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.Write(format, arg);
+            System.Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Writes the specified string value, followed by the current line terminator, to the 
+        /// standard output stream, in the specified color.
+        /// </summary>
+        /// <param name="color">The foreground color to display the value in.</param>
+        /// <param name="value">The value to write.</param>
+        public static void WriteLine(ConsoleColor color, string value)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.WriteLine(value);
+            System.Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Writes the text representation of the specified array of objects, followed by the current 
+        /// line terminator, to the standard output stream using the specified format information, in 
+        /// the specified color.
+        /// </summary>
+        /// <param name="color">The foreground color to display the value in.</param>
+        /// <param name="format">A composite format string.</param>
+        /// <param name="arg">An array of objects to write using <paramref name="format"/>.</param>
+        public static void WriteLine(ConsoleColor color, string format, params object[] arg)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.WriteLine(format, arg);
+            System.Console.ResetColor();
+        }
+    }
+}
