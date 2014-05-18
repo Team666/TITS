bcd734fe47b4a3073bab91043af56f4535facd9e
Philipe Louchtch - de Raadt <cool2002405@msn.com>
2014-05-18 22:13:34 +0200

    Initial UI messing around

---
+++ Source/faceTITS/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TITS;
+
+namespace faceTITS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static TITS.Components.NowPlaying _player;
+
+        public static TITS.Components.NowPlaying Player
+        {
+            get
+            {
+                if (_player == null)
+                {
+                    _player = new TITS.Components.NowPlaying();
+                }
+                return _player;
+            }
+        }
+
+    }
+}
