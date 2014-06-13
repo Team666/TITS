9ec04328a381c52c6f03dd38dba5bf48b6343421
Philipe Louchtch - de Raadt <cool2002405@msn.com>
2014-06-13 23:56:52 +0200

    Added basic folder picker | fixed #15

--- Source/faceTITS/MainWindow.xaml.cs
+++ Source/faceTITS/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace faceTITS
 {
@@ -26,10 +27,40 @@ namespace faceTITS
             this.InitializeComponent();
         }
 
+        private string SelectDirectory()
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.Title = "Select music directory to play";
+            dialog.IsFolderPicker = true;
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+            dialog.AddToMostRecentlyUsedList = false;
+            dialog.AllowNonFileSystemItems = false;
+            dialog.EnsureFileExists = true;
+            dialog.EnsurePathExists = true;
+            dialog.EnsureReadOnly = false;
+            dialog.EnsureValidNames = true;
+            dialog.Multiselect = false;
+            dialog.ShowPlacesList = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                return dialog.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
         private void window_Loaded(object sender, RoutedEventArgs e)
         {
             App.Player.Playlist = new TITS.Library.Playlist();
-            App.Player.Playlist.AddFromDirectory(@"P:\Music\Greatest Hits");
+
+            var path = SelectDirectory();
+            App.Player.Playlist.AddFromDirectory(path);
+
+
             //App.Player.StartPlaying();
 
             this.MinWidth  = this.Width;
